8ff417aec5802a93f01f88b4bc51ae504d0b6bd9
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-06-16 21:44:43 +0530

    added postinstall script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl:"https://mysterious-coast-24504.herokuapp.com",
+  baseUrl:"https://stark-cliffs-39688.herokuapp.com",
   baseLoginUrl:"https://teeming.online"
 };
 
