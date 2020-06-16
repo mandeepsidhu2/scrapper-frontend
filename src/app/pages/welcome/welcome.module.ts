e2cded433c807cb6e53019155b151cfcd3d540a0
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-06-16 12:35:18 +0530

    added basic card structure

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
-
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { WelcomeComponent } from './welcome.component';
 
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule,NzCardModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
