39e1a527e698879e5ff25a611faf044f3aed6e9d
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-06-17 00:12:06 +0530

    added login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-
+import { Router } from '@angular/router';
+import { SocialAuthService } from "angularx-social-login";
+import {  GoogleLoginProvider } from "angularx-social-login";
+import {CookieService} from "ngx-cookie-service"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +11,43 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 export class AppComponent {
   isCollapsed = false;
   topic:any;
+  userName:any;
+  imageUrl:any;
+  userData:any
   constructor(
-    private route: ActivatedRoute,
-    private router:Router
+    private router:Router,
+    private authService: SocialAuthService,
+    private cookieService: CookieService
   ) {}
+  loggedIn:Boolean=false;
+  ngOnInit() {
+    this.loggedIn=atob(this.cookieService.get(btoa('loggedIn')))=='true'?true:false;
+   if(this.loggedIn){
+    this.userName=this.cookieService.get('name')
+    this.imageUrl=this.cookieService.get('imageUrl')
+   }
+  }
+  signInWithGoogle(): void {
+    this.userData=this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(data=>{
+      this.cookieService.set(btoa('loggedIn'), btoa('true'));
+      this.loggedIn=true;
+      this.cookieService.set('name',data['name'])
+      this.cookieService.set('imageUrl',data['photoUrl'])
+      this.userName=this.cookieService.get('name')
+      this.imageUrl=this.cookieService.get('imageUrl')
+      console.log(this.cookieService.get('name'))
+      return data
+    });
+   
+  }
+  signOut(): void {
+    this.cookieService.set(btoa('loggedIn'), btoa('false'));
+    this.cookieService.delete('name')
+    this.cookieService.delete('loggedIn')
+    this.cookieService.delete('imageUrl')
+    this.loggedIn=false;
+    this.authService.signOut();
+  }
   search(event:any){
     if(event.keyCode==13){
       console.log(event,this.topic)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,9 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-
+import { SocialLoginModule,SocialAuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 registerLocaleData(en);
 
 @NgModule({
@@ -23,6 +25,7 @@ registerLocaleData(en);
     AppComponent
   ],
   imports: [
+    SocialLoginModule,
     BrowserModule,
     AppRoutingModule,
     IconsProviderModule,
@@ -32,9 +35,25 @@ registerLocaleData(en);
     HttpClientModule,
     BrowserAnimationsModule,
     NzInputModule,
-    NzButtonModule
+    NzButtonModule,
+    NzAvatarModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [{ provide: NZ_I18N, useValue: en_US },
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '148434873376-a1k8ubdj3g3oqkh53an00v8angbj2itd.apps.googleusercontent.com'
+            ),
+          }
+        ],
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
